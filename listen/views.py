dcd19facd2b232cfa96ef3b9cf28f30d0ace5de7
guillermocoding <gs8rj@virginia.edu>
2020-12-19 18:57:16 -0500

    Update

--- listen/views.py
+++ listen/views.py
@@ -3,6 +3,8 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 
+TOKEN = "BQD0G2MDxEneP34RN74X-ZJyd0eQwCAXUG9wbG12omnrWoApdJmmdM7G4j3EzAEb57jj-08GxJOsT9GwPBBnDkIzmIIjPLmWIh9cqsadArIjwHHr_VdN3z_yUVspPiHy8BVXdyU3w4gvH0-3jRE7vW1aGB_iLiESUqmpA_6zh-YT28_nQnnnMmw42SXk4HQ09vwHdU5BoEisyW1nf4-SdswZl4mgH2azVZQ35n0oo_gJaPljMNQYT-QWeRX_-de03xwNZzVbPR5nsxUUYBzn9_VyxwCMqG9Q0Saw1Lo"
+
 # Create your views here.
 def dashboard(request):
     NATURE_TRACKS = [
@@ -20,7 +22,7 @@ def dashboard(request):
           url,
           headers={
             "Content-Type":"application/json",
-            "Authorization":"Bearer BQAkoALbVnAioLjVipoUVXDpjIptIU5DX9UVOF749l7HJknLIU7b9whPtjho4h_Y519Nij7sVoW00_mok2K2jOhSypdOs5MHg8xD9w_GVD9s9K_7_khXuWBRnHQsTbKdrzHqkoLWpmgpiCoHbnJRhtmGoeAtrdGGRyv2wFC3OzomQXoeDNi2UxqDdoCZXo6cuLuk1hcoAUXE22VbByA2GGIqpmVbS1sHj4DbqKk6FwguR_eFyVs1K-y7tbxiIg7oc7Ll7itxvRgaYX24tOer69kvbMYmOnlIjbOtEY0"
+            "Authorization":"Bearer "+TOKEN
           }
         )
         dashboard_tracks.append(response.json())
@@ -34,7 +36,20 @@ def nature(request):
       '0ggwBO4x4XWDYHDnCpz3TJ',
       '0Q7oSvv9r8xRxqOJc3uWte'
     ]
-    return render(request, 'listen/nature.html', { 'nature_tracks': NATURE_TRACKS })
+
+    dashboard_tracks = []
+
+    for track in NATURE_TRACKS:
+        url = "https://api.spotify.com/v1/tracks/{}".format(track)
+        response = requests.get(
+          url,
+          headers={
+            "Content-Type":"application/json",
+            "Authorization":"Bearer "+TOKEN
+          }
+        )
+        dashboard_tracks.append(response.json())
+    return render(request, 'listen/nature.html', { 'nature_tracks': dashboard_tracks})
 
 def office(request):
     NATURE_TRACKS = [
@@ -43,14 +58,41 @@ def office(request):
       '43xZo7aQ7UxTIacmAWx7Rw',
       '7lzpN2K7zbaq1iYCElsnMe'
     ]
-    return render(request, 'listen/office.html', { 'nature_tracks': NATURE_TRACKS })
+    dashboard_tracks = []
+
+    for track in NATURE_TRACKS:
+        url = "https://api.spotify.com/v1/tracks/{}".format(track)
+        response = requests.get(
+          url,
+          headers={
+            "Content-Type":"application/json",
+            "Authorization":"Bearer "+TOKEN
+          }
+        )
+        dashboard_tracks.append(response.json())
+    return render(request, 'listen/office.html', { 'nature_tracks': dashboard_tracks})
 
 def public(request):
     NATURE_TRACKS = [
-      '1y7pIwI5ocoutzF66TIBk9',
-      '7lakKIL70bIDxfj2umABVS',
-      '1H7zXMcEx0PPW62NCSHbUV',
-      '0ggwBO4x4XWDYHDnCpz3TJ',
-      '0Q7oSvv9r8xRxqOJc3uWte'
+      '7gT0l4iDWrguvyyvwIOCi1',
+      '2yY7B5cRxjoNymsaJjqMua',
+      '2Fz11dL4CfITHw0GDt91bG',
+      '26sy9DDRDqvJkvYxmKLhz0',
+      '26sy9DDRDqvJkvYxmKLhz0',
+      '26sy9DDRDqvJkvYxmKLhz0',
     ]
-    return render(request, 'listen/public.html', { 'nature_tracks': NATURE_TRACKS })
+
+    dashboard_tracks = []
+
+    for track in NATURE_TRACKS:
+        url = "https://api.spotify.com/v1/tracks/{}".format(track)
+        response = requests.get(
+          url,
+          headers={
+            "Content-Type":"application/json",
+            "Authorization":"Bearer "+TOKEN
+          }
+        )
+        dashboard_tracks.append(response.json())
+
+    return render(request, 'listen/public.html', { 'nature_tracks': dashboard_tracks})
