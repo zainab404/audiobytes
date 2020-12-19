ea633e8757a53341b908cf008e1d0a522538c01d
guillermocoding <gs8rj@virginia.edu>
2020-12-19 17:45:24 -0500

    Adds side and display tracks

--- listen/urls.py
+++ listen/urls.py
@@ -5,5 +5,8 @@ from . import views
 APP_NAME = 'listen'
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('dashboard/', views.dashboard, name='dashboard'),
+  path('nature/', views.nature, name='nature'),
+  path('office/', views.office, name='office'),
+  path('public/', views.public, name='public'),
 ]
--- listen/views.py
+++ listen/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def dashboard(request):
     NATURE_TRACKS = [
       '1y7pIwI5ocoutzF66TIBk9',
       '7lakKIL70bIDxfj2umABVS',
@@ -10,5 +10,33 @@ def index(request):
       '0ggwBO4x4XWDYHDnCpz3TJ',
       '0Q7oSvv9r8xRxqOJc3uWte'
     ]
-    return render(request, 'listen/index.html', { 'nature_tracks': NATURE_TRACKS })
+    return render(request, 'listen/dashboard.html', { 'nature_tracks': NATURE_TRACKS })
 
+def nature(request):
+    NATURE_TRACKS = [
+      '1y7pIwI5ocoutzF66TIBk9',
+      '7lakKIL70bIDxfj2umABVS',
+      '1H7zXMcEx0PPW62NCSHbUV',
+      '0ggwBO4x4XWDYHDnCpz3TJ',
+      '0Q7oSvv9r8xRxqOJc3uWte'
+    ]
+    return render(request, 'listen/nature.html', { 'nature_tracks': NATURE_TRACKS })
+
+def office(request):
+    NATURE_TRACKS = [
+      '1FlbLSX8Fat3z4jLQkTAvf',
+      '635hEgWYUnjbvQr0YTxzyj',
+      '43xZo7aQ7UxTIacmAWx7Rw',
+      '7lzpN2K7zbaq1iYCElsnMe'
+    ]
+    return render(request, 'listen/office.html', { 'nature_tracks': NATURE_TRACKS })
+
+def public(request):
+    NATURE_TRACKS = [
+      '1y7pIwI5ocoutzF66TIBk9',
+      '7lakKIL70bIDxfj2umABVS',
+      '1H7zXMcEx0PPW62NCSHbUV',
+      '0ggwBO4x4XWDYHDnCpz3TJ',
+      '0Q7oSvv9r8xRxqOJc3uWte'
+    ]
+    return render(request, 'listen/public.html', { 'nature_tracks': NATURE_TRACKS })
